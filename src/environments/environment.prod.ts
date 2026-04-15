7a7358d7673c1769ebbd156d91172ee3be2df46e
PrIyank12Singh <115387653+PrIyank12Singh@users.noreply.github.com>
2026-04-15 22:11:34 +0530

    [Priyank] added updated files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apigateway-jl2d.onrender.com/api',
+  apiUrl: 'https://apigateway-jl2d.onrender.com',
   authUrl: 'https://apigateway-jl2d.onrender.com/api/auth',
   quantityUrl: 'https://apigateway-jl2d.onrender.com/api/quantity'
 };
